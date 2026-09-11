eaea9d00e8a0a677842ae34df1cea23a959ade89
Syed Muhammad Yahya <syedyahya308@gmail.com>
2026-09-11 11:34:03 +0500

    Fixing API URL for vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 //const API_URL = "http://localhost:5000";
 //const API_URL = import.meta.env.VITE_API_URL;
-const API_URL = `${window.location.protocol}//${window.location.hostname}:5000`;
+const API_URL = import.meta.env.VITE_API_URL;
 export async function apiRequest(endpoint, options = {}) {
   const response = await fetch(`${API_URL}${endpoint}`, {
     ...options,
